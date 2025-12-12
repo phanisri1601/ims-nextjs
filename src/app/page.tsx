92c77f0fa0e5f48ae513d1e62f5b0bb04881a07b
rakshitha-creator <rakshitha@imsolutions.mobi>
2025-12-12 09:47:25 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import WhyChooseUs from "@/components/WhyChooseUs";
 import WantToKnowMore from "@/components/WantToKnowMore";
 import BlogsSection from "@/components/BlogsSection";
 import CareerSection from "@/components/CareerSection";
-import CursorRipple from "@/components/CursorRipple";
 
 import Clients from "@/components/Clients";
 import FAQ from "@/components/FAQ";
@@ -13,7 +12,6 @@ import FAQ from "@/components/FAQ";
 export default function Home() {
   return (
     <main>
-      <CursorRipple />
       <Hero />
       <VideoSection />
       <AboutSection />
