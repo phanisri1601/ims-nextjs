b421a761920f9d1cb9f8014d632d272740d2acd8
phani-Dcoder <phanisri@imsolutions.mobi>
2026-07-15 12:12:09 +0530

    after the updates

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,11 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-<<<<<<< HEAD
-/// <reference types="next/navigation-types/compat/navigation" />
 import "./.next/dev/types/routes.d.ts";
-=======
-import "./.next/types/routes.d.ts";
->>>>>>> 466eb4cf0f4dc84a4882e4857cd2ec28c8639698
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
